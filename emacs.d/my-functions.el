3418850ca4583cdd84e073e1aac262562de4b915
Winton DeShong (flex) <wdeshong@andculture.com>
2011-10-07 16:05:31 -0400

    emacs setup

---
+++ emacs.d/my-functions.el
@@ -0,0 +1,9 @@
+;;
+;; Collection of custom functions for use in Emacs
+;;
+(defun iwb ()
+  "indent whole buffer"
+  (interactive)
+  (delete-trailing-whitespace)
+  (indent-region (point-min) (point-max) nil)
+  (untabify (point-min) (point-max)))
