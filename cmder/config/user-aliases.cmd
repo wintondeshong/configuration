f20460b872f4dcc71fc0466a2b626e9e121b89b9
Winton DeShong <wdeshong@andculture.com>
2016-09-08 15:47:52 -0400

    Updated cmder aliases to use the latest format

---
+++ cmder/config/user-aliases.cmd
@@ -0,0 +1,43 @@
+;= @echo off
+;= rem Call DOSKEY and use this file as the macrofile
+;= %SystemRoot%\system32\doskey /listsize=1000 /macrofile=%0%
+;= rem In batch mode, jump to the end of the file
+;= goto:eof
+;= Add aliases below here
+e.=explorer .
+gl=git log --oneline --all --graph --decorate  $*
+ls=ls --show-control-chars -F --color $*
+pwd=cd
+clear=cls
+history=cat %CMDER_ROOT%\config\.history
+unalias=alias /d $1
+vi=vim $*
+cmderr=cd /d "%CMDER_ROOT%"
+e.=explorer .
+gl=git log --oneline --all --graph --decorate  $*
+ls=ls --show-control-chars -F --color $*
+pwd=cd
+clear=cls
+history=cat %CMDER_ROOT%\config\.history
+unalias=alias /d $1
+
+getty=cd C:\Clients\gettysburg $*
+trytn=cd C:\Clients\Trytn\trytn $*
+anthm=cd C:\Clients\anthm\anthm $*
+a=cd C:\Clients\anthm\anthm $*
+anthmecommerce=cd C:\Clients\anthm\anthm.modules.ecommerce $*
+aecom=cd C:\Clients\anthm\anthm.modules.ecommerce $*
+anthmlogs=cd C:\Clients\anthm\anthm.site\Modules\Anthm.Modules.Log4Net\Log $*
+anthmmedia=cd C:\Clients\anthm\anthm.modules.media $*
+amedia=cd C:\Clients\anthm\anthm.modules.media $*
+anthmpages=cd C:\Clients\anthm\anthm.modules.pages $*
+apages=cd C:\Clients\anthm\anthm.modules.pages $*
+anthmpayments=cd C:\Clients\anthm\anthm.integrations.payments $*
+apayments=cd C:\Clients\anthm\anthm.integrations.payments $*
+anthmsite=cd C:\Clients\anthm\anthm.site $*
+anthmstorage=cd C:\Clients\anthm\anthm.modules.storage $*
+arel=cd C:\Clients\anthm\anthm.modules.releases $*
+asite=cd C:\Clients\anthm\anthm.site $*
+astorage=cd C:\Clients\anthm\anthm.modules.storage $*
+
+ll=ls -alt $*
