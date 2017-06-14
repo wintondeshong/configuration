203466ee0635434c2a2fb23b1fef8a7a0f642e91
Winton DeShong <wdeshong@andculture.com>
2017-06-13 22:13:30 -0400

    Added powershell script to remap caps lock to ctl on windows

---
+++ powershell/windows-remap-caps-to-ctl.ps1
@@ -0,0 +1,5 @@
+$hexified = "00,00,00,00,00,00,00,00,02,00,00,00,1d,00,3a,00,00,00,00,00".Split(',') | % { "0x$_"};
+
+$kbLayout = 'HKLM:\System\CurrentControlSet\Control\Keyboard Layout';
+
+New-ItemProperty -Path $kbLayout -Name "Scancode Map" -PropertyType Binary -Value ([byte[]]$hexified);
